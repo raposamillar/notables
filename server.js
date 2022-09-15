2295d22fe08b7db02ebf0cad49172c324bb37f7d
raposamillar <lisa.raposamillar@gmail.com>
2022-09-15 07:34:45 -0400

    add middleware to server.js

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+app.use(require('./routes'));
 
 app.listen(PORT, () => { 
   console.log(`Now listening on http://localhost:${PORT}`);
