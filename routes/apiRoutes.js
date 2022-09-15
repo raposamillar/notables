b5024a1dee24cbac14c7e2dd385b9bc870754411
raposamillar <lisa.raposamillar@gmail.com>
2022-09-15 07:28:56 -0400

    add GET and POST routes in apiRoutes.js

---
+++ routes/apiRoutes.js
@@ -0,0 +1,18 @@
+const { writeFile } = require('fs');
+const router = require('express').Router();
+let store = require('../db/db.json');
+const { v1: uuid1 } = require('uuid');
+
+router.get('/notes', (req, res) => res.json(store));
+
+router.post('/notes', (req, res) => {
+  req.body.id = uuid1();
+
+  store.push(req.body)
+  writeFile('./db/db.json', JSON.stringify(store), err => {
+    if (err) throw err;
+    res.json(store);
+  })
+});
+
+module.exports = router;
