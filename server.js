7bddeef99d8b7619ff5941f692e4acfdc733617c
raposamillar <lisa.raposamillar@gmail.com>
2022-09-15 07:11:06 -0400

    set environment variable on line 4 in server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 
-app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT}`));
+app.listen(PORT, () => { 
+  console.log(`Now listening on http://localhost:${PORT}`);
+});  
