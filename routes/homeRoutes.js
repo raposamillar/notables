5dc5b1b14957aec218def31d7a55d017da0631d9
raposamillar <lisa.raposamillar@gmail.com>
2022-09-15 07:01:35 -0400

    add path and GET routes in homeRoutes.js

---
+++ routes/homeRoutes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+
+// Handle directory and file paths
+const path = require('path');
+
+router.get('/', (req, res) => 
+  // Join arguments and send to the index.html file in the public directory
+  res.sendFile(path.join(__dirname, '../public/index.html')));
+
+router.get('/notes', (req, res) => 
+  // Join arguments and send to the notes.html file in the public directory
+  res.sendFile(path.join(__dirname, '../public/notes.html')));
+
+// router.get('*', (req, res) => 
+//   res.sendFile(path.join(__dirname, '../public/index.html')));
+
+module.exports = router;  
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,10 @@
 const router = require('express').Router();
+
+// Create variables to access JS files
 const apiRoutes = require('./apiRoutes');
 const homeRoutes = require('./homeRoutes');
 
+router.use('/api', require('./apiRoutes'));
+router.use('/', require('./homeRoutes'));
+
+module.exports = router;
